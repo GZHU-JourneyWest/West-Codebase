4ac4d5a19fbc911f2dfeb60f904d302a04f58d6f
lightyears <lightyears1998@hotmail.com>
2018-04-19 19:17:34 +0800

    升级KMP算法

--- Raw/基础与经验/字符串KMP.cpp
+++ Raw/基础与经验/字符串KMP.cpp
@@ -1,16 +1,24 @@
-int pre[maxN]; fill(pre, pre+maxN, 0);
-char s[maxN], p[maxN]; scanf("%s%s", s+1, p+1);
+int pre[maxN]; 
+char s[maxN], p[maxN];  // 文本串、模板串 
 
-for (int i = 2, j = 0; p[i]; ++i)  // 取得pre数组
-{
-	while (j > 0 && p[i] != p[j+1]) j = pre[j];
-	if (p[i] == p[j+1]) ++j;
-	pre[i] = j;
+void prepare() {
+	fill(pre, pre+maxN, -1);
+	for (int i=1, j=-1; p[i]; ++i)  
+	{
+		while (j>=0 && p[i] != p[j+1]) j = pre[j];
+		if (p[i] == p[j+1]) ++j;
+		pre[i] = j;
+	}
 }
 
-for (int i = 1, j = 0; s[i]; ++i)  // 开始匹配
+void kmp(vector<int> &match)
 {
-	while (j > 0 && s[i] != p[j+1]) j = pre[j];
-	if (s[i] == p[j+1]) ++j;
-	if (!p[j+1]) ++cnt; // 匹配成功
+	match.clear(); prepare();
+	for (int i=0, j=-1; s[i]; ++i) {
+		while (j>=0 && s[i] != p[j+1]) j = pre[j];
+		if (s[i] == p[j+1]) ++j;
+		if (!p[j+1]) {  // 匹配成功 
+			match.push_back(i-j);
+		}
+	}
 }
